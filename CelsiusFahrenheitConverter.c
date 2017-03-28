953eb530c403f6601f728432d27ca673ab4b4083
Luis Felipe <luisfelipekai@gmail.com>
2017-03-27 22:28:18 -0300

    Implement with For statement.

--- CelsiusFahrenheitConverter.c
+++ CelsiusFahrenheitConverter.c
@@ -4,6 +4,22 @@
     print Fahrenheit-Celsius table for fahr = 0, 20, ..., 300
 */
 void main(){
+    /* FOR LOOP*/
+    int fahr;
+    printf("\nUsing For loop\n\n");
+    printf("Fahrenheit-Celsius table\n");
+    for (fahr = 0; fahr <= 300; fahr = fahr + 20){
+        printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr - 32));
+    }
+    
+    int celsius;
+    printf("\nCelsius-Fahrenheit table\n");
+    for (celsius = 0; celsius <= 100; celsius = celsius + 10){
+        printf("%3d %6.1f\n", celsius, (9.0 * celsius + 160)/5.0);
+    }
+    /* WHILE LOOP 
+
+    printf("\nUsing While loop\n");
     float fahr, celsius;
     int lower, upper, step;
 
@@ -23,8 +39,9 @@ void main(){
     upper = 100;
     printf("\nCelsius-Fahrenheit table\n");
     while (celsius <= upper){
-        fahr = (9 * celsius + 160) / 5;
+        fahr = (9.0 * celsius + 160.0) / 5.0;
         printf("%3.0f %6.1f\n", celsius, fahr);
         celsius = celsius + 10;
     }
+    */
 }
