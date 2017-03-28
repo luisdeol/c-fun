c8c149635fc141478d77513a987fa723b45c680d
Luis Felipe <luisfelipekai@gmail.com>
2017-03-27 22:15:17 -0300

    Generate Celsius-Fahrenheit table.

--- CelsiusFahrenheitConverter.c
+++ CelsiusFahrenheitConverter.c
@@ -4,7 +4,7 @@
     print Fahrenheit-Celsius table for fahr = 0, 20, ..., 300
 */
 void main(){
-    int fahr, celsius;
+    float fahr, celsius;
     int lower, upper, step;
 
     lower = 0;
@@ -12,9 +12,19 @@ void main(){
     step = 20;
 
     fahr = lower;
+    printf("Fahrenheit-Celsius table\n");
     while(fahr <= upper){
-        celsius = 5 * (fahr - 32) / 9;
-        printf("%d\t%d\n", fahr, celsius);
+        celsius = 5.0 * (fahr - 32.0) / 9.0;
+        printf("%3.0f %6.1f\n", fahr, celsius);
         fahr = fahr + step;
+    }
+
+    celsius = 0.0;
+    upper = 100;
+    printf("\nCelsius-Fahrenheit table\n");
+    while (celsius <= upper){
+        fahr = (9 * celsius + 160) / 5;
+        printf("%3.0f %6.1f\n", celsius, fahr);
+        celsius = celsius + 10;
     }
 }
